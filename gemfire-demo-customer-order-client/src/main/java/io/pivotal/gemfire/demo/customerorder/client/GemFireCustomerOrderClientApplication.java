5b8064aebfd12fd2b15d79973222ddf0b3097fd3
jcherng-pivotal <jcherng@pivotal.io>
2016-07-26 17:34:35 -0500

    added swagger-ui integration

--- gemfire-demo-customer-order-client/src/main/java/io/pivotal/gemfire/demo/customerorder/client/GemFireCustomerOrderClientApplication.java
+++ gemfire-demo-customer-order-client/src/main/java/io/pivotal/gemfire/demo/customerorder/client/GemFireCustomerOrderClientApplication.java
@@ -5,9 +5,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ImportResource;
 import org.springframework.context.annotation.PropertySource;
 
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
 @SpringBootApplication
 @ImportResource("classpath:customer-order-gfc-context.xml")
 @PropertySource("classpath:application-gfc.properties")
+@EnableSwagger2
 public class GemFireCustomerOrderClientApplication {
 
 	public static void main(String[] args) {
